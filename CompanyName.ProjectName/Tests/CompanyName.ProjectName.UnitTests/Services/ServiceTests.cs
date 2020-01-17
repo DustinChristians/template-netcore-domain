ec792400955870f3b4af3cee2206f042b6abad14
Dustin <dustinchristians@gmail.com>
2020-01-16 19:01:53 -0500

    Added a unit-test for the TestService.

---
+++ CompanyName.ProjectName/Tests/CompanyName.ProjectName.UnitTests/Services/ServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using CompanyName.ProjectName.Infrastructure.Services;
+using NUnit.Framework;
+
+namespace CompanyName.ProjectName.UnitTests
+{
+    [TestFixture]
+    public class ServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var testService = new TestService();
+            var result = testService.GetTestModel();
+
+            Assert.IsTrue(string.Equals(result.Name, "Hello, World!"));
+        }
+    }
+}
