33936bf09e1c51ad402ab7f3147fe0be60f65fc4
Dustin <dustinchristians@gmail.com>
2020-02-20 08:11:04 -0500

    Added a database initializer for creating some test content.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Repository/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using CompanyName.ProjectName.Repository.Models;
+
+namespace CompanyName.ProjectName.Repository.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(CompanyNameProjectNameContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any messages.
+            if (context.Messages.Any())
+            {
+                return; // DB has been seeded
+            }
+
+            // load test data into arrays rather than List<T> collections to optimize performance.
+            var messages = new Message[]
+            {
+                new Message { Text = "Hello, World!" },
+            };
+
+            foreach (Message s in messages)
+            {
+                context.Messages.Add(s);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
