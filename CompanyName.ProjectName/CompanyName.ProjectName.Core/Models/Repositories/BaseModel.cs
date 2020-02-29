fe58a2e74d14774c66107e5f86a6c604815289ba
Dustin <dustinchristians@gmail.com>
2020-02-29 08:19:29 -0500

    Added a base repository model.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Models/Repositories/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CompanyName.ProjectName.Core.Models.Repositories
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public int ModifiedBy { get; set; }
+    }
+}
