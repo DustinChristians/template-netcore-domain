2cd564e9f9a394ce38023c08967b602f1b7c026a
Dustin <dustinchristians@gmail.com>
2020-03-06 06:14:03 -0500

    Added object extension methods.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CompanyName.ProjectName.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool AllPropertiesAreNull<T>(this T obj)
+        {
+            return obj == null || typeof(T).GetProperties().All(propertyInfo => propertyInfo.GetValue(obj) == null);
+        }
+    }
+}
