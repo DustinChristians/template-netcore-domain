f8d29fb5d0d5071bf2dc7e9b1ad41b2c7a90bd5e
Dustin <dustinchristians@gmail.com>
2020-03-06 06:14:42 -0500

    Added the user repository model.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Models/Repositories/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyName.ProjectName.Core.Models.Repositories
+{
+    public class User : BaseModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+            = new List<Message>();
+    }
+}
