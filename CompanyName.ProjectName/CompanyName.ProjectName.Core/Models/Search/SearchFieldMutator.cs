dacc05973f9cb5fdfff21f9ed3206feea1289091
Dustin <dustinchristians@gmail.com>
2020-03-06 06:18:11 -0500

    Added search generics for searching, filtering and sorting to reduce duplicated code.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Models/Search/SearchFieldMutator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CompanyName.ProjectName.Core.Models.Search
+{
+    public delegate IQueryable<TItem> QueryMutator<TItem, TSearch>(IQueryable<TItem> items, TSearch search);
+
+    public class SearchFieldMutator<TItem, TSearch>
+    {
+        public SearchFieldMutator(Predicate<TSearch> condition, QueryMutator<TItem, TSearch> mutator)
+        {
+            Condition = condition;
+            Mutator = mutator;
+        }
+
+        public Predicate<TSearch> Condition { get; set; }
+        public QueryMutator<TItem, TSearch> Mutator { get; set; }
+
+        public IQueryable<TItem> Apply(TSearch search, IQueryable<TItem> query)
+        {
+            return Condition(search) ? Mutator(query, search) : query;
+        }
+    }
+}
---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Models/Search/SearchMutator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyName.ProjectName.Core.Models.Search
+{
+    public class SearchMutator<TItem, TSearch>
+    {
+        public List<SearchFieldMutator<TItem, TSearch>> SearchFieldMutators { get; set; } = new List<SearchFieldMutator<TItem, TSearch>>();
+
+        public void AddCondition(Predicate<TSearch> condition, QueryMutator<TItem, TSearch> mutator)
+        {
+            SearchFieldMutators.Add(new SearchFieldMutator<TItem, TSearch>(condition, mutator));
+        }
+
+        public IQueryable<TItem> Apply(TSearch search, IQueryable<TItem> query)
+        {
+            foreach (var searchFieldMutator in SearchFieldMutators)
+            {
+                query = searchFieldMutator.Apply(search, query);
+            }
+
+            return query;
+        }
+    }
+}
