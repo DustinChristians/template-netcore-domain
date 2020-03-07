42505d6dd8a7a806f8f5954b146a943046b5ff04
Dustin <dustinchristians@gmail.com>
2020-03-07 07:34:51 -0500

    Added the users repository.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Abstractions/Repositories/IUsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CompanyName.ProjectName.Core.Models.Repositories;
+using CompanyName.ProjectName.Core.Models.ResourceParameters;
+
+namespace CompanyName.ProjectName.Core.Abstractions.Repositories
+{
+    public interface IUsersRepository : IBaseRepository<User>
+    {
+        Task<IEnumerable<User>> GetUsersAsync(UsersResourceParameters parameters);
+    }
+}
