80f3f38b92ac2d373ecff56c1de937b5b6ab3581
Dustin <dustinchristians@gmail.com>
2020-03-08 12:58:27 -0400

    Added the users service.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Abstractions/Services/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿using CompanyName.ProjectName.Core.Abstractions.Repositories;
+
+namespace CompanyName.ProjectName.Core.Abstractions.Services
+{
+    public interface IUsersService
+    {
+        public IUsersRepository UsersRepository { get; }
+    }
+}
---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Infrastructure/Services/UsersService.cs
@@ -0,0 +1,15 @@
+﻿using CompanyName.ProjectName.Core.Abstractions.Repositories;
+using CompanyName.ProjectName.Core.Abstractions.Services;
+
+namespace CompanyName.ProjectName.Infrastructure.Services
+{
+    public class UsersService : IUsersService
+    {
+        public UsersService(IUsersRepository usersRepository)
+        {
+            this.UsersRepository = usersRepository;
+        }
+
+        public IUsersRepository UsersRepository { get; }
+    }
+}
