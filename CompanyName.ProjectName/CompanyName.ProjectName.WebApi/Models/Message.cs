db90dd1caef2b4204bb81d663c41aba34d6bb070
Dustin <dustinchristians@gmail.com>
2020-04-09 06:39:29 -0400

    Fixed a capitalization issue.

--- CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Models/Message.cs
+++ CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Models/Message.cs
@@ -5,7 +5,7 @@ namespace CompanyName.ProjectName.WebApi.Models
     public class Message
     {
         public int Id { get; set; }
-        public int userId { get; set; }
+        public int UserId { get; set; }
         public string Text { get; set; }
         public int ChannelId { get; set; }
         public DateTime CreatedOn { get; set; }
