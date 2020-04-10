bf02f205d53c965df5c8a8ff2b2fdeadf66d227d
Dustin <dustinchristians@gmail.com>
2020-04-10 13:18:41 -0400

    Updated the formatting of the ID validation attribute.

--- CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Attributes/Validation/IdValidationAttribute.cs
+++ CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Attributes/Validation/IdValidationAttribute.cs
@@ -1,15 +1,17 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace CompanyName.ProjectName.WebApi.Attributes.Validation
 {
     public class Id : ValidationAttribute
     {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        protected override ValidationResult IsValid(
+            object value,
+            ValidationContext validationContext)
         {
             return Convert.ToInt32(value) > 0 ?
                 ValidationResult.Success :
-                new ValidationResult($"{validationContext.DisplayName} must have a value greater than 0.");
+                new ValidationResult($"{validationContext.DisplayName} must be an integer greater than 0.");
         }
     }
 }
