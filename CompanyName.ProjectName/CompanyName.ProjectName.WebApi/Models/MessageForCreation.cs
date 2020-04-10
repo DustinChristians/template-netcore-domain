f9cc41c48b39fc2f09e3b8bfbb9f4bba87a63544
Dustin <dustinchristians@gmail.com>
2020-04-10 13:34:20 -0400

    Added the Id validation attribute to the message for creation model.

--- CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Models/MessageForCreation.cs
+++ CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Models/MessageForCreation.cs
@@ -1,14 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using CompanyName.ProjectName.WebApi.Attributes.Validation;
 
 namespace CompanyName.ProjectName.WebApi.Models
 {
     public class MessageForCreation
     {
-        [Required]
+        [Required, Id]
         public int UserId { get; set; }
         [Required]
         public string Text { get; set; }
-        [Required]
+        [Required, Id]
         public int ChannelId { get; set; }
     }
 }
