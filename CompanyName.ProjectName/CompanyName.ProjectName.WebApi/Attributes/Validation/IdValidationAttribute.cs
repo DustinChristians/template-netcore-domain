d9f1d1f60586fe68edc3e8de5a79135d832b6073
Dustin <dustinchristians@gmail.com>
2020-04-10 12:39:20 -0400

    Added an ID validation attribute.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Attributes/Validation/IdValidationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyName.ProjectName.WebApi.Attributes.Validation
+{
+    public class Id : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return Convert.ToInt32(value) > 0 ?
+                ValidationResult.Success :
+                new ValidationResult($"{validationContext.DisplayName} must have a value greater than 0.");
+        }
+    }
+}
