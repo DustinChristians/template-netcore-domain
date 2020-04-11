80bbed80b75c0bad1e413684cd4178ec195930de
Dustin <dustinchristians@gmail.com>
2020-04-11 14:02:10 -0400

    Added a new user mapping profile to the repository project.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Repository/MappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CompanyName.ProjectName.Core.Models.Domain;
+using CompanyName.ProjectName.Repository.Entities;
+
+namespace CompanyName.ProjectName.Repository.MappingProfiles
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<UserEntity, User>().ReverseMap();
+        }
+    }
+}
