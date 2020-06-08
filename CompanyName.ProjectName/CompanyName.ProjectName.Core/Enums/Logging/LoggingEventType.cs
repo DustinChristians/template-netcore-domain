0bf697dafdaf7e1e87a0ce5fcf8e008c8b8dd5a5
Dustin <dustinchristians@gmail.com>
2020-06-08 18:59:30 -0400

    Added a LoggingEventType enum for all the available log event types.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Enums/Logging/LoggingEventType.cs
@@ -0,0 +1,43 @@
+﻿namespace CompanyName.ProjectName.Core.Enums.Logging
+{
+    public enum LoggingEventType
+    {
+        /// <summary>
+        /// Contain the most detailed messages. These messages may contain sensitive app data.
+        /// These messages are disabled by default and should not be enabled in production.
+        /// </summary>
+        Trace,
+
+        /// <summary>
+        /// For debugging and development. Use with caution in production due to the high volume.
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        /// Tracks the general flow of the app. May have long-term value.
+        /// </summary>
+        Information,
+
+        /// <summary>
+        /// For abnormal or unexpected events. Typically includes errors or conditions that don't
+        /// cause the app to fail.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// For errors and exceptions that cannot be handled. These messages indicate a failure in
+        /// the current operation or request, not an app-wide failure.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// For failures that require immediate attention. Examples: data loss scenarios, out of disk space.
+        /// </summary>
+        Critical,
+
+        /// <summary>
+        /// Specifies that a logging category should not write any messages.
+        /// </summary>
+        None
+    }
+}
