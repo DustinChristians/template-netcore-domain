b4c852cae50ea182999cc12c8aa91cef219fed39
Dustin <dustinchristians@gmail.com>
2020-06-09 07:55:44 -0400

    Added a logger service interface.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Abstractions/Services/Logging/ILoggerService.cs
@@ -0,0 +1,9 @@
+﻿using CompanyName.ProjectName.Core.Models.Logging;
+
+namespace CompanyName.ProjectName.Core.Abstractions.Services.Logging
+{
+    public interface ILoggerService
+    {
+        void Log(LogEntry entry);
+    }
+}
