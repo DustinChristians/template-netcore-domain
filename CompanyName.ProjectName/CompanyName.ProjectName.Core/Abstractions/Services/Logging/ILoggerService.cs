68cdedc3b7adade1ab5ad7a8ed350e90cf678367
Dustin <dustinchristians@gmail.com>
2020-06-09 07:55:44 -0400

    Revert "Added a logger service interface."
    This reverts commit b4c852cae50ea182999cc12c8aa91cef219fed39.

--- CompanyName.ProjectName/CompanyName.ProjectName.Core/Abstractions/Services/Logging/ILoggerService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CompanyName.ProjectName.Core.Models.Logging;
-
-namespace CompanyName.ProjectName.Core.Abstractions.Services.Logging
-{
-    public interface ILoggerService
-    {
-        void Log(LogEntry entry);
-    }
-}
