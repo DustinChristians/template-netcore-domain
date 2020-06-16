3d38bbf744901e69c9cee0ed948a07d5d38be7a3
Dustin <dustinchristians@gmail.com>
2020-06-16 06:20:19 -0400

    Added an Event Log repository.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Abstractions/Repositories/Logging/IEventLogRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CompanyName.ProjectName.Core.Abstractions.Repositories.Logging
+{
+    public interface IEventLogRepository
+    {
+        void DeleteLogsOlderThanDateTime(DateTime dateTime);
+    }
+}
---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Repository/Repositories/Logging/EventLogRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CompanyName.ProjectName.Core.Abstractions.Repositories.Logging;
+using CompanyName.ProjectName.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyName.ProjectName.Repository.Repositories.Logging
+{
+    public class EventLogRepository : IEventLogRepository
+    {
+        protected CompanyNameProjectNameContext context;
+
+        public EventLogRepository(CompanyNameProjectNameContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteLogsOlderThanDateTime(DateTime dateTime)
+        {
+            context.Database.ExecuteSqlRaw("DELETE FROM EventLog WHERE TimeStamp < {0}", dateTime);
+        }
+    }
+}
