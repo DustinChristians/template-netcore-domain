18cd6fd13d66597052f63c9f41d4862fa5c9e4d4
Dustin <dustinchristians@gmail.com>
2020-06-16 06:24:03 -0400

    Added a task scheduler to register and schedule tasks throughout the application.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.WebApi/Scheduler/TaskScheduler.cs
@@ -0,0 +1,24 @@
+﻿using CompanyName.ProjectName.Core.Abstractions.Tasks.Logging;
+using CompanyName.ProjectName.Infrastructure.Tasks.Logging;
+using Hangfire;
+
+namespace CompanyName.ProjectName.WebApi.Scheduler
+{
+    public class TaskScheduler
+    {
+        private readonly IDatabaseEventLogCleanupTask databaseEventLogCleanupTask;
+
+        public TaskScheduler(IDatabaseEventLogCleanupTask databaseEventLogCleanupTask)
+        {
+            this.databaseEventLogCleanupTask = databaseEventLogCleanupTask;
+        }
+
+        public static void ScheduleRecurringTasks()
+        {
+            RecurringJob.RemoveIfExists(nameof(DatabaseEventLogCleanupTask.DeleteOldEventLogs));
+            RecurringJob.AddOrUpdate<IDatabaseEventLogCleanupTask>(nameof(DatabaseEventLogCleanupTask),
+            task => task.DeleteOldEventLogs(),
+            "0 */5 * ? * *");
+        }
+    }
+}
