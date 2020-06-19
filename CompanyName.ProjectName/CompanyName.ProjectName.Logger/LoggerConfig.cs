350cb5047c6a3996d145928ddc04dff81f96bf5a
Dustin <dustinchristians@gmail.com>
2020-06-19 18:43:46 -0400

    Added a Logger library to contain all of the Serilog specific logging code and dependencies.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Logger/LoggerConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace CompanyName.ProjectName.Logger
+{
+    public class LoggerConfig
+    {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+        .Build();
+
+        public static Serilog.ILogger CreateLogger()
+        {
+            return new LoggerConfiguration()
+                // Add/Update configuration settings in appsettings.json. Don't add them here.
+                .ReadFrom.Configuration(Configuration)
+                .Enrich.FromLogContext()
+                .CreateLogger();
+        }
+
+        public static void AddDependencies(IServiceCollection services)
+        {
+            // Give the Serilog CorrelationId Enricher access to the requests HttpContext so that it
+            // can attach the correlation ID to the request/response.
+            services.AddHttpContextAccessor();
+        }
+
+        public static void Configure(ILoggerFactory loggerFactory)
+        {
+            // Add Serilog to the Logging Pipeline
+            loggerFactory.AddSerilog();
+        }
+    }
+}
