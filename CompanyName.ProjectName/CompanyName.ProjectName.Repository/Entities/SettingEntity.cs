f2e19b38395432cafa80e656160919e4114a0a3d
Dustin <dustinchristians@gmail.com>
2020-07-05 15:48:04 -0400

    Added a setting entity.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Repository/Entities/SettingEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CompanyName.ProjectName.Repository.Entities
+{
+    public class SettingEntity : BaseEntity
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Type { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+    }
+}
