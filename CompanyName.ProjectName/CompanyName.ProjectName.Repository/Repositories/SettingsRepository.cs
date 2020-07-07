34f93327b8dabb726fbdcd15f9819ce918be1cc0
Dustin <dustinchristians@gmail.com>
2020-07-07 06:14:59 -0400

    Added a settings repository.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Repository/Repositories/SettingsRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using CompanyName.ProjectName.Core.Models.Domain;
+using CompanyName.ProjectName.Repository.Data;
+using CompanyName.ProjectName.Repository.Entities;
+
+namespace CompanyName.ProjectName.Repository.Repositories.Settings
+{
+    public class SettingsRepository : BaseRepository<Setting, SettingEntity>
+    {
+        public SettingsRepository(CompanyNameProjectNameContext context, IMapper mapper)
+            : base(context, mapper)
+        {
+        }
+
+        public async Task<T> GetSetting<T>(string key, T defaultValue)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                return defaultValue;
+            }
+
+            var setting = await FirstOrDefaultAsync(x => x.Key == key);
+
+            return setting == null ? defaultValue : (T)Convert.ChangeType(setting.Value, typeof(T));
+        }
+    }
+}
