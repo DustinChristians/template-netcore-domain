1336eae21a05c50c265135b770a952df29bbe3d6
Dustin <dustinchristians@gmail.com>
2020-07-08 06:28:58 -0400

    Added an async try/get return model because async methods can't use ref or out variables.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Models/Helpers/AsyncTryGetResult.cs
@@ -0,0 +1,8 @@
+﻿namespace CompanyName.ProjectName.Core.Models.Helpers
+{
+    public class AsyncTryGetResult<T>
+    {
+        public bool Successful { get; set; }
+        public T Value { get; set; }
+    }
+}
