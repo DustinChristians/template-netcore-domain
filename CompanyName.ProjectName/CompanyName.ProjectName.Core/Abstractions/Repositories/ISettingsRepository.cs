3a80fa9068c84bce600b29a18ba6c9334a4a0dbd
Dustin <dustinchristians@gmail.com>
2020-07-09 06:20:04 -0400

    Added try/get setting and try/set setting methods.

---
+++ CompanyName.ProjectName/CompanyName.ProjectName.Core/Abstractions/Repositories/ISettingsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using CompanyName.ProjectName.Core.Models.Domain;
+using CompanyName.ProjectName.Core.Models.Helpers;
+
+namespace CompanyName.ProjectName.Core.Abstractions.Repositories
+{
+    public interface ISettingsRepository : IBaseRepository<Setting>
+    {
+        Task<T> GetSettingValue<T>(string key, T defaultValue);
+        Task<AsyncTryGetResult<T>> TryGetSettingValue<T>(string key);
+        Task<bool> TryUpdateSettingValue<T>(string key, T value);
+    }
+}
--- CompanyName.ProjectName/CompanyName.ProjectName.Repository/Repositories/SettingsRepository.cs
+++ CompanyName.ProjectName/CompanyName.ProjectName.Repository/Repositories/SettingsRepository.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AutoMapper;
+using CompanyName.ProjectName.Core.Abstractions.Repositories;
 using CompanyName.ProjectName.Core.Models.Domain;
+using CompanyName.ProjectName.Core.Models.Helpers;
 using CompanyName.ProjectName.Repository.Data;
 using CompanyName.ProjectName.Repository.Entities;
 
 namespace CompanyName.ProjectName.Repository.Repositories.Settings
 {
-    public class SettingsRepository : BaseRepository<Setting, SettingEntity>
+    public class SettingsRepository : BaseRepository<Setting, SettingEntity>, ISettingsRepository
     {
         public SettingsRepository(CompanyNameProjectNameContext context, IMapper mapper)
             : base(context, mapper)
         {
         }
 
-        public async Task<T> GetSetting<T>(string key, T defaultValue)
+        public async Task<T> GetSettingValue<T>(string key, T defaultValue)
         {
             if (string.IsNullOrWhiteSpace(key))
             {
@@ -24,6 +26,50 @@ namespace CompanyName.ProjectName.Repository.Repositories.Settings
             var setting = await FirstOrDefaultAsync(x => x.Key == key);
 
             return setting == null ? defaultValue : (T)Convert.ChangeType(setting.Value, typeof(T));
+        }
+
+        public async Task<AsyncTryGetResult<T>> TryGetSettingValue<T>(string key)
+        {
+            var result = new AsyncTryGetResult<T>
+            {
+                Successful = false
+            };
+
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                return result;
+            }
+
+            var setting = await FirstOrDefaultAsync(x => x.Key == key);
+
+            if (setting == null)
+            {
+                return result;
+            }
+
+            result.Value = (T)Convert.ChangeType(setting.Value, typeof(T));
+            result.Successful = true;
+
+            return result;
+        }
+
+        public async Task<bool> TryUpdateSettingValue<T>(string key, T value)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                return false;
+            }
+
+            var setting = await FirstOrDefaultAsync(x => x.Key == key);
+
+            if (setting == null)
+            {
+                return false;
+            }
+
+            setting.Value = value.ToString();
+
+            return true;
         }
     }
 }
