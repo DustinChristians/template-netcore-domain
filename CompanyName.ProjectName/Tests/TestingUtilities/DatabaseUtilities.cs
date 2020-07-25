0b8e0919a6b25980a66bb8cac133f359d76153b0
Dustin <dustinchristians@gmail.com>
2020-07-25 09:50:46 -0400

    Added a GetTestDbConextOptions utility method for in memory test databases.

---
+++ CompanyName.ProjectName/Tests/TestingUtilities/DatabaseUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyName.ProjectName.TestUtilities
+{
+    public static class DatabaseUtilities
+    {
+        public static DbContextOptions<TContext> GetTestDbConextOptions<TContext>() where TContext : DbContext
+        {
+            // Random Guids are used for database names so in memory databases aren't
+            // reused between tests to ensure test isolation.  
+            return new DbContextOptionsBuilder<TContext>()
+                            .UseInMemoryDatabase(new Guid().ToString())
+                            .Options;
+        }
+    }
+}
