7e6da1acb181083b091cf8f3d1027d15545be7cc
Dustin <dustinchristians@gmail.com>
2020-07-25 08:51:25 -0400

    Added a GetTestMapper utility method.

---
+++ CompanyName.ProjectName/Tests/TestingUtilities/MapperUtilities.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using AutoMapper.Extensions.ExpressionMapping;
+
+namespace TestingUtilities
+{
+    public static class MapperUtilities
+    {
+        public static IMapper GetTestMapper()
+        {
+            var mappingConfig = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.AddMaps("CompanyName.ProjectName.Infrastructure");
+                    cfg.AddMaps("CompanyName.ProjectName.Repository");
+                    cfg.AddExpressionMapping();
+                });
+
+            var mapper = mappingConfig.CreateMapper();
+
+            return mapper;
+        }
+    }
+}
