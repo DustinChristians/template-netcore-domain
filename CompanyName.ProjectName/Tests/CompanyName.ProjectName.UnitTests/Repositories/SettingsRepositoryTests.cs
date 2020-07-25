c05d7873b098dea71ec32724e7ac8fb96a120ae8
Dustin <dustinchristians@gmail.com>
2020-07-25 08:41:28 -0400

    Added a unit test for GetSettingValue.

--- CompanyName.ProjectName/Tests/CompanyName.ProjectName.UnitTests/Repositories/SettingsRepositoryTests.cs
+++ CompanyName.ProjectName/Tests/CompanyName.ProjectName.UnitTests/Repositories/SettingsRepositoryTests.cs
@@ -1,7 +1,14 @@
-﻿using NUnit.Framework;
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.Extensions.ExpressionMapping;
+using CompanyName.ProjectName.Repository.Data;
+using CompanyName.ProjectName.Repository.Repositories.Settings;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 
 namespace CompanyName.ProjectName.UnitTests.Repositories
 {
+    [TestFixture]
     public class SettingsRepositoryTests
     {
         [SetUp]
@@ -10,9 +17,36 @@ namespace CompanyName.ProjectName.UnitTests.Repositories
         }
 
         [Test]
-        public void GetSettingValue()
+        public async Task GetSettingValue()
         {
-            Assert.Pass();
+            // Arrange
+            var options = new DbContextOptionsBuilder<CompanyNameProjectNameContext>()
+                .UseInMemoryDatabase("CompanyNameProjectNameDatabaseForTesting")
+                .Options;
+
+            using (var context = new CompanyNameProjectNameContext(options))
+            {
+                var testSetting = new Repository.Entities.SettingEntity()
+                {
+                    Key = "TestKey",
+                    Value = "TestValue",
+                    Type = typeof(string).ToString(),
+                    DisplayName = "Test Key",
+                    Description = "For Testing GetSettingValue"
+                };
+
+                context.Settings.Add(testSetting);
+
+                context.SaveChanges();
+
+                var settingsRepository = new SettingsRepository(context, GetTestMapper());
+
+                // Act
+                var value = await settingsRepository.GetSettingValue("TestKey", string.Empty);
+
+                // Assert
+                Assert.AreEqual(value, testSetting.Value);
+            }
         }
 
         [Test]
@@ -25,6 +59,21 @@ namespace CompanyName.ProjectName.UnitTests.Repositories
         public void TryUpdateSettingValue()
         {
             Assert.Pass();
+        }
+
+        private static IMapper GetTestMapper()
+        {
+            var mappingConfig = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.AddMaps("CompanyName.ProjectName.Infrastructure");
+                    cfg.AddMaps("CompanyName.ProjectName.Repository");
+                    cfg.AddExpressionMapping();
+                });
+
+            var mapper = mappingConfig.CreateMapper();
+
+            return mapper;
         }
     }
 }
