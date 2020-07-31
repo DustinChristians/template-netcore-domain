ab18a6adb678f5b15fca4f1d766debd462957836
Dustin <dustinchristians@gmail.com>
2020-07-31 06:37:20 -0400

    Added the Logger Provider for EF Core Logging.

---
+++ CompanyName.ProjectName/Tests/TestingUtilities/LogToActionLoggerProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyName.ProjectName.TestUtilities
+{
+    public class LogToActionLoggerProvider : ILoggerProvider
+    {
+        private readonly Action<string> efCoreLogAction;
+        private readonly LogLevel logLevel;
+
+        public LogToActionLoggerProvider(
+            Action<string> efCoreLogAction,
+            LogLevel logLevel = LogLevel.Information)
+        {
+            this.efCoreLogAction = efCoreLogAction;
+            this.logLevel = logLevel;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new EFCoreLogger(this.efCoreLogAction, logLevel);
+        }
+
+        public void Dispose()
+        {
+            // nothing to dispose
+        }
+    }
+}
